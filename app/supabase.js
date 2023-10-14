3970f04a8b81d9fef7cace40ff307929bf32aecc
GIFT2105 <letuka.gift21@gmail.com>
2023-10-15 01:29:11 +0200

    local env file added

--- app/supabase.js
+++ app/supabase.js
@@ -1,7 +1,7 @@
 // supabase.js
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://kivdpfahaajtdiynmmsj.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpdmRwZmFoYWFqdGRpeW5tbXNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY0Njg2MDQsImV4cCI6MjAxMjA0NDYwNH0.zmgDKVno_Mf8c9GYkepnSoHaGuCqQLgUUorfpmnrB-Q';
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
